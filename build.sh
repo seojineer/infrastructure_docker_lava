bd939d1162c4f065a4f696d170a6ecb77a0f3bc3
seoji <seoji@nexell.co.kr>
2019-01-03 10:44:44 +0900

    modify docker build script
    modify docker build script
    
    Signed-off-by: seoji <seoji@nexell.co.kr>

--- build.sh
+++ build.sh
@@ -20,8 +20,8 @@ else
     echo "Error!!"
 fi
 
-docker image build ${CACHE_OPTION} -t nexelldocker/lava-server:2018.11 -f Dockerfile-server .
+docker build ${CACHE_OPTION} -t nexelldocker/lava-server:2018.11 -f Dockerfile-server .
 echo "---------------------------------------------------------------"
 echo "---------------------------------------------------------------"
 echo "---------------------------------------------------------------"
-docker image build ${CACHE_OPTION} -t nexelldocker/lava-slave:2018.11 -f Dockerfile-slave .
+docker build ${CACHE_OPTION} -t nexelldocker/lava-slave:2018.11 -f Dockerfile-slave .
